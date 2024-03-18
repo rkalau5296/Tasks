933d2b0d9105efc51620096c42b032f006a35e7a
Rafał Kalata <rafal.kalata.itservice@gmail.com>
2024-03-18 23:51:40 +0100

    commit

---
+++ Tasks/Core/ViewModels/TasksViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Tasks.Core.Models;
+using Tasks.Core.Models.Domains;
+using Task = Tasks.Core.Models.Domains.Task;
+
+namespace Tasks.Core.ViewModels;
+public class TasksViewModel
+{
+    public IEnumerable<Task> Tasks { get; set; }
+    public IEnumerable<Category> Categories { get; set; }
+    public FilterTasks FilterTasks { get; set; }
+}
+
